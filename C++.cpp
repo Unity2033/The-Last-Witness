6276be6062902fc3fc44145e5bddef983f9f17b8
mol1986@naver.com <kileab@naver.com>
2022-10-14 21:47:43 +0900

    2022.10.14 (클래스의 생성자와 소멸자)

--- C++.cpp
+++ C++.cpp
@@ -11,13 +11,20 @@
 class Phone 
 {
 public :
-    std::string name;
-
-protected :
-    void Call()
+    // 생성자는 자신의 클래스 이름으로 만들어주면 됩니다.
+    // 생성자는 반환형이 존재하지 않습니다.
+    Phone()
     {
-        std::cout << "전화 걸기" << std::endl;
+        std::cout << "핸드폰이 생성되었습니다." << std::endl;
     }
+
+    // 소멸자는 자신의 클래스 이름 앞에 ~를 사용해서 만들어주면 됩니다.
+    ~Phone()
+    {
+        std::cout << "핸드폰이 파괴되었습니다." << std::endl;
+    }
+
+    std::string name;
 
     void Message()
     {
@@ -26,6 +33,12 @@ protected :
         // 상위 클래스는 하위 클래스에서 정의한 함수를 사용할 수 없습니다.
         // Touch();
     }
+
+protected :
+    void Call()
+    {
+        std::cout << "전화 걸기" << std::endl;
+    } 
 };
 
 // 상속하는 방법
@@ -42,6 +55,7 @@ public :
 int main()
 {
     // C++ 동적 할당
+    /*
     // new (힙 영역에 할당할 메모리 공간의 크기)
     int * ptr = new int;
 
@@ -71,7 +85,31 @@ int main()
     }
 
     delete [] arrptr;
+    */
 
+    // Phone phone; // <- 스택 
+
+    Phone * phone = new Phone(); // <- 힙
+     
+    delete phone;
+    // SmartPhone * sPhone = new SmartPhone();
+
+    // 정적 바인딩
+    // 배열의 크기는 컴파일 시점에 메모리 크기가 결정됩니다.
+    // 배열[100]; <- 80~90개 메모리 공간은 낭비를 하게 되는거죠
+    
+    // 특정 시점에 배열의 크기를 100개 사용해야 되지만,
+    // 그 특정 시점이 넘어가면 더이상 사용할 필요가 없을 때
+  
+    // 동적 할당은 실행 시간에 메모리 공간 확보하고 줄이고 할 수 있습니다.
+    // 리스트
+
+    // 생성자
+    // 클래스의 객체가 생성되었을 때 객체를 초기화하는 
+    // 목적으로 자동으로 호출되는 함수입니다.
+
+    // 소멸자
+    // 클래스의 객체가 소멸되었을 때 자동으로 호출되는 함수입니다.
 
     return 0;
 }
