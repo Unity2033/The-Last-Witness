b1366782ab02d0dace1ba6c2121ec4c884486589
mol1986@naver.com <kileab@naver.com>
2022-11-21 21:47:42 +0900

    (연결리스트) 2022.11.21

---
+++ (선택 정렬) 2022.11.18.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class User
+{
+public:
+	void operator() ()
+	{
+		cout << "Function Object" << endl;
+		cout << "signal : " << signal << endl;
+	}
+
+private:
+	int signal = 0;
+};
+
+int main()
+{
+	// 선택 정렬
+	/*
+	// 주어진 리스트 중에서 최솟값을 찾은 다음 그 값을 맨 앞에
+	// 위치한 값과 교체하는 정렬입니다.
+	int data[5] = { 10,3,4,2,1 };
+
+	for (int i = 0; i < 5; i++)
+	{
+		int minIndex = i;
+
+		for (int j = i; j < 5; j++)
+		{
+			if (data[minIndex] > data[j])
+			{
+				minIndex = j;
+			}
+		}
+
+		int temp = data[i];
+		data[i] = data[minIndex];
+		data[minIndex] = temp;
+	}
+
+	for (int element : data)
+	{
+		cout << element << endl;
+	}
+	*/
+
+	// 함수 객체
+	// 함수처럼 동작하는 객체입니다.
+	User user;
+	user();
+
+	return 0;
+}
+
+
--- C++.cpp
+++ C++.cpp
@@ -2,55 +2,51 @@
 
 using namespace std;
 
-class User
+struct Node
 {
-public :
-	void operator() ()
-	{
-		cout << "Function Object" << endl;
-		cout << "signal : " << signal << endl;
-	}
-
-private:
-	int signal = 0;
+	int data;
+	Node * next;
 };
 
 int main()
 {	
-	// 선택 정렬
-	/*
-	// 주어진 리스트 중에서 최솟값을 찾은 다음 그 값을 맨 앞에
-	// 위치한 값과 교체하는 정렬입니다.
-	int data[5] = { 10,3,4,2,1 };
+	// 연결리스트
 
-	for (int i = 0; i < 5; i++)
+	// 1. 단방향 연결 리스트
+    Node * head = NULL;
+
+	//	00B056A2 <- 00B056A2
+    Node * node1 = new Node;
+	head = node1;
+
+	head->data = 10;
+
+	//  00AD15D8 <- 00AD15D8
+	Node * node2 = new Node;
+	
+	head->next = node2;
+	node2->data = 20;
+
+	Node * node3 = new Node;
+	head->next = node3;
+	node3->data = 30;
+
+	node3->next = NULL;
+
+	Node * currentPtr = head;
+
+	while (currentPtr != NULL)
 	{
-		int minIndex = i;
-
-		for (int j = i; j < 5; j++)
-		{
-			if (data[minIndex] > data[j])
-			{
-				minIndex = j;
-			}
-		}
-
-		int temp = data[i];
-		data[i] = data[minIndex];
-		data[minIndex] = temp;
+		cout << currentPtr->data << endl;
+		currentPtr = currentPtr->next;
 	}
 
-	for (int element : data)
-	{
-		cout << element << endl;
-	}
-	*/
 
-	// 함수 객체
-	// 함수처럼 동작하는 객체입니다.
-	User user;
-	user();
+	// 2. 원형 연결 리스트
 
+	// 3. 양방향 연결 리스트
+
+	
 	return 0;
 }
 
