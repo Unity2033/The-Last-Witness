53aab869388217b7cf99b74bd2adb4c76ed01143
mol1986@naver.com <kileab@naver.com>
2022-11-23 21:47:16 +0900

    (이중 연결 리스트) 2022.11.23

--- C++.cpp
+++ C++.cpp
@@ -5,43 +5,84 @@ using namespace std;
 struct Node
 {
 	int data;
+	Node * prev;
 	Node * next;
 };
+
+Node * head = NULL;
+Node * tail = NULL;
 
 void Insert(Node * targetNode, int value)
 {
 	Node * newNode = new Node;
 	newNode->data = value;
+
+	newNode->prev = targetNode;
 	newNode->next = targetNode->next;
-	
+
+	targetNode->next->prev = newNode;
 	targetNode->next = newNode;
 }
 
-void Remove(Node * originNode)
+void Remove(Node * targetNode)
 {
-	Node* targetNode = originNode->next;
-	originNode->next = targetNode->next;
-	delete targetNode;
+	Node * deleteNode = targetNode->next;
+
+	if (deleteNode == NULL)
+	{
+		return;
+	}
+
+	deleteNode->next->prev = targetNode;
+	targetNode->next = deleteNode->next;
+
+	delete deleteNode;
 }
 
-
-int main()
-{	
-	Node * head = new Node();
-	head->next = NULL;
-
-	Insert(head, 10);
-	Insert(head, 20);
-	Insert(head, 30);
-	Remove(head);
-
+void FrontOutput()
+{
 	Node * currentNode = head->next;
 
-	while (currentNode != NULL)
+	while (currentNode != tail)
 	{
 		cout << currentNode->data << endl;
 		currentNode = currentNode->next;
 	}
+}
+
+void BackOutput()
+{
+	Node * currentNode = tail->prev;
+
+	while (currentNode != head)
+	{
+		cout << currentNode->data << endl;
+		currentNode = currentNode->prev;
+	}
+}
+
+int main()
+{	
+	     // 더미 노드 1 
+	head = new Node;
+
+	     // 더미 노드 2
+	tail = new Node;
+
+	head->next = tail;
+	head->prev = NULL;
+
+	tail->next = NULL;
+	tail->prev = head;
+
+	Insert(head, 10);
+	Insert(head, 20);
+	Insert(head, 30);
+
+	Remove(head);
+
+	FrontOutput();
+	BackOutput();
 
 
 
