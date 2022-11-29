8dc9e9368df87782e1f358e2cf76e7356de233bd
mol1986@naver.com <kileab@naver.com>
2022-11-29 21:47:10 +0900

    (이진 트리) 2022.11.29

--- C++.cpp
+++ C++.cpp
@@ -5,67 +5,64 @@ using namespace std;
 struct Node
 {
 	int data;
-	Node * next;
+	Node * left;
+	Node * right;
 };
 
-Node * FrontInsert(Node * head, int value)
+Node * CreateNode(int value, Node * leftPtr, Node * rightPtr)
 {
 	Node * newNode = new Node;
-	newNode->data = value;
 
-	if (head == NULL)
-	{
-		head = newNode;
-		newNode->next = head;
-	}
-	else
-	{
-		newNode->next = head->next;
-		head->next = newNode;
-	}
-	
-	return head;
+	newNode->data = value;
+	newNode->left = leftPtr;
+	newNode->right = rightPtr;
+
+	return newNode;
 }
 
-Node * BackInsert(Node * head, int value)
+// 전위 순회
+// 1. 자기 자신부터 출력 
+// 2. 왼쪽의 자식 노드를 방문(출력)
+// 3. 오른쪽의 자식 노드를 방문(출력)
+void Preorder(Node * root)
 {
-	Node * newNode = new Node;
-	newNode->data = value;
-
-	if (head == NULL)
+	if (root != NULL)
 	{
-		head = newNode;
-		newNode->next = head;
+	   // 값 출력
+	   // 왼쪽 자식 노드 방문 
+	   // 오른쪽 자식 노드 방문
 	}
-	else
-	{
-		newNode->next = head->next;
-		head->next = newNode;
-		head = newNode;
-	}
-
-	return head;
 }
+
+// 중위 순회
+// 1. 왼쪽 자식 노드를 출력
+// 2. 자기 자신의 노드를 출력
+// 3. 오른쪽 자식 노드를 출력
+
+// 후위 순회
+// 1. 왼쪽 자식 노드를 출력
+// 2. 오른쪽 자식 노드를 출력
+// 3. 자기 자신의 노드를 출력합니다. 
 
 int main()
 {
-	// 원형 연결 리스트
-	Node * head = NULL;
+	// 트리 구조란?
+	// 한 노드에서 시작해서 다른 정점들을
+	// 순회하여 자기 자신에게 돌아오는 순환이
+	// 없는 연결 그래프입니다.
 
-	head = FrontInsert(head, 100);
-	head = FrontInsert(head, 200);
-	head = BackInsert(head, 500);
+	// 이진 트리
+	// 최대 2개의 자식 노드를 가지는 트리입니다.
 
-	Node * currentNode = head->next;
+	Node * n7 = CreateNode(7, NULL, NULL);
+	Node * n6 = CreateNode(6, NULL, NULL);
+	Node * n5 = CreateNode(5, NULL, NULL);
+	Node * n4 = CreateNode(4, NULL, NULL);
 
-	while (currentNode != head)
-	{
-		cout << currentNode->data << endl;
-		currentNode = currentNode->next;
-	}
-
-	cout << currentNode->data << endl;
-
+	Node * n3 = CreateNode(3, n6, n7);
+	Node * n2 = CreateNode(2, n4, n5);
+	Node * n1 = CreateNode(1, n2, n3);
+	
 
 	return 0;
 }
