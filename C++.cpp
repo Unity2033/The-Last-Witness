146534155426e342aa7501365b5bb7b0b288594b
mol1986@naver.com <kileab@naver.com>
2022-12-02 21:45:50 +0900

    (계수 정렬) 2022.12.2

--- C++.cpp
+++ C++.cpp
@@ -1,60 +1,30 @@
 #include <iostream>
 
 using namespace std;
-#define MEMORY 100
 
-struct Node
-{
-	int data[MEMORY];
-	int size = 0;
-};
-
-void Insert(Node * root, int value)
-{
-	int index;
-
-	// 힙에 데이터가 꽉 찬 상태라면 return
-	if (root->size >= MEMORY - 1)
-	{
-		return;
-	}
-
-	root->size++;
-	index = root->size;
-	root->data[index] = value;
-
-	// 종료되는 조건
-	// 1. 루트 노드일 때
-	// 2. 부모 노드의 값이 클 때 
-
-	//                   	// 부모 노드 1(10)		자식 노드 2 20
-	while ((index != 1) && (root->data[index/2] < root->data[index]))
-	{
-		swap(root->data[index / 2], root->data[index]);
-	}
-
-}
 
 int main()
 {	
-	// 힙
-	// 최댓값과 최솟값을 찾아내는 연산을 
-	// 빠르게 하기 위해 고안된 완전 이진 트리를
-	// 기본으로 한 자료구조입니다.
-	Node heap;
+	// 계수 정렬
+	// 데이터의 값을 직접 비교하지 않고,
+	// 단순하게 각 숫자가 몇 개 있는지
+	// 갯수를 세어 저장한 다음 정렬하는 알고리즘입니다.
 
-	// 10
-	// 20
-	// 30
-	Insert(&heap, 10);
-	Insert(&heap, 20);
-	Insert(&heap, 30);
+	int array[10] = { 1, 5, 4, 1, 2, 4, 3, 3, 3, 5 };
+	int count[5] = { 0, };
 
-	for (int i = 1; i <= 3; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		cout << heap.data[i] << endl;
+		count[array[i] - 1] += 1;
 	}
 
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < count[i]; j++)
+		{
+			cout << i + 1 << " ";
+		}
+	}
 
 	return 0;
 }
