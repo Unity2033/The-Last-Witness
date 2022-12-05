006891d694c2e31b59bb7dfa15989af51b0c2e25
mol1986@naver.com <kileab@naver.com>
2022-12-05 21:48:58 +0900

    (해쉬 테이블) 2022.12.5

--- C++.cpp
+++ C++.cpp
@@ -2,29 +2,55 @@
 
 using namespace std;
 
+struct Node
+{
+	int key;
+	int value;
+	Node * next;
+};
+
+struct Bucket
+{
+	Node * head;
+	int size;
+};
+
+
+Node * CreateNode(int key, int value)
+{
+	Node * newNode = new Node;
+	newNode->key = key;
+	newNode->value = value;
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+int HashFunction(int key)
+{
+	return key % 5;
+}
+
+void Insert(int key, int value)
+{
+	//      0           10 % 5
+	int hashIndex = HashFunction(key);
+    
+	// 방금 생성한 새로운 노드의 주소가 반환됩니다.
+	struct Node * newNode = CreateNode(key, value);
+	
+
+
+ 
+}
 
 int main()
 {	
-	// 계수 정렬
-	// 데이터의 값을 직접 비교하지 않고,
-	// 단순하게 각 숫자가 몇 개 있는지
-	// 갯수를 세어 저장한 다음 정렬하는 알고리즘입니다.
-
-	int array[10] = { 1, 5, 4, 1, 2, 4, 3, 3, 3, 5 };
-	int count[5] = { 0, };
-
-	for (int i = 0; i < 10; i++)
-	{
-		count[array[i] - 1] += 1;
-	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < count[i]; j++)
-		{
-			cout << i + 1 << " ";
-		}
-	}
+	// 해시 테이블
+	// 해시함수를 사용해서 변환한 값을
+	// index로 삼아서 key와 value를 저장하는
+	// 자료구조입니다.
+	Bucket * hash = new Bucket[5];
 
 	return 0;
 }
