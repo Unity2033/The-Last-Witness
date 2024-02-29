8c7d509dec70cbb580ee5c1e51b8a3defc12038d
Unity2033 <kileab@naver.com>
2024-02-29 19:46:05 +0900

    (2024.2.29) 피격 효과 시스템

---
+++ Dodge Game/Assets/Scripts/FlashMaterial.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashMaterial : MonoBehaviour
+{
+    [SerializeField] Material flashMaterial;
+
+    private Material originMaterial;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        originMaterial = spriteRenderer.material;
+    }
+
+    public IEnumerator HitEffect(float duration)
+    {
+        spriteRenderer.material = flashMaterial;
+
+        yield return new WaitForSeconds(duration);
+
+        spriteRenderer.material = originMaterial;
+    }
+}
--- Dodge Game/Assets/Scripts/Player.cs
+++ Dodge Game/Assets/Scripts/Player.cs
@@ -5,37 +5,59 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animator animator;
+    private Rigidbody2D rigidBody2D;
     [SerializeField] Vector2 direction;
-    [SerializeField] float speed = 1.0f;
+    [SerializeField] float speed = 500.0f;
     private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        Keyboard();
+    }
+
+    void FixedUpdate()
+    {
         Move();
+
+        Reverse();
+    }
+
+    void Keyboard()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
     }
 
     void Move()
     {
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.y = Input.GetAxisRaw("Vertical");
-
-        if(direction.x < 0)
+        if(rigidBody2D.velocity == Vector2.zero)
+        {
+            animator.SetBool("Run", false);
+        }
+        else
         {
             animator.SetBool("Run", true);
+        }
+
+        rigidBody2D.velocity = new Vector3(direction.x, direction.y, 0) * speed * Time.fixedDeltaTime;
+    }
+
+    void Reverse()
+    {
+        if (direction.x < 0)
+        {
             spriteRenderer.flipX = false;
         }
-        else if(direction.x > 0)
+        else if (direction.x > 0)
         {
-            animator.SetBool("Run", true);
             spriteRenderer.flipX = true;
         }
-
-        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
