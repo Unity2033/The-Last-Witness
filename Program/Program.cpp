0b6b2530de443dc9a7829dfb6a3545a8f6225b71
Kileab <kileab@naver.com>
2025-08-10 11:40:55 +0900

    Project Design & Library Configuration
    - Installing the SFML library x86 and setting the path

---
+++ Program/Program.cpp
@@ -0,0 +1,32 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "The Last Witness");
+
+    window.clear(sf::Color(sf::Color::Green));
+
+    window.display();
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                {
+                    window.close();
+                }
+            }
+        }
+    }
+
+
+    return 0;
+}
+
